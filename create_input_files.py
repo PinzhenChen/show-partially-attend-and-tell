eca1a444bb9a2e50ea1d8d0fa973c9e6ee78e86e
Change19961103 <s1508767@sms.ed.ac.uk>
2019-02-17 16:04:41 +0000

    baseline code

--- create_input_files.py
+++ create_input_files.py
@@ -3,9 +3,9 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='flickr8k',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption data/',
+                       karpathy_json_path='dataset_flickr8k.json',
+                       image_folder='Flicker8k_Dataset',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption data/',
+                       output_folder='output',
                        max_len=50)
--- train.py
+++ train.py
@@ -5,14 +5,14 @@ import torch.utils.data
 import torchvision.transforms as transforms
 from torch import nn
 from torch.nn.utils.rnn import pack_padded_sequence
-from models import Encoder, DecoderWithAttention
+from models import Encoder, DecoderWithAttention, DecoderWithoutAttention
 from datasets import *
 from utils import *
 from nltk.translate.bleu_score import corpus_bleu
 
 # Data parameters
-data_folder = '/media/ssd/caption data'  # folder with data files saved by create_input_files.py
-data_name = 'coco_5_cap_per_img_5_min_word_freq'  # base name shared by data files
+data_folder = 'dataset'  # folder with data files saved by create_input_files.py
+data_name = 'flickr8k_5_cap_per_img_5_min_word_freq'  # base name shared by data files
 
 # Model parameters
 emb_dim = 512  # dimension of word embeddings
@@ -26,7 +26,7 @@ cudnn.benchmark = True  # set to true only if inputs to model are fixed size; ot
 start_epoch = 0
 epochs = 120  # number of epochs to train for (if early stopping is not triggered)
 epochs_since_improvement = 0  # keeps track of number of epochs since there's been an improvement in validation BLEU
-batch_size = 32
+batch_size = 16
 workers = 1  # for data-loading; right now, only 1 works with h5py
 encoder_lr = 1e-4  # learning rate for encoder if fine-tuning
 decoder_lr = 4e-4  # learning rate for decoder
@@ -52,6 +52,11 @@ def main():
 
     # Initialize / load checkpoint
     if checkpoint is None:
+        # decoder = DecoderWithoutAttention(attention_dim=attention_dim,
+        #                                embed_dim=emb_dim,
+        #                                decoder_dim=decoder_dim,
+        #                                vocab_size=len(word_map),
+        #                                dropout=dropout)
         decoder = DecoderWithAttention(attention_dim=attention_dim,
                                        embed_dim=emb_dim,
                                        decoder_dim=decoder_dim,
